cfdf4be4394f61f06b68dfe6346c45027c222e9a
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-02-16 19:37:16 +0100

    fix problem with static files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -14,7 +14,7 @@ CLOUDINARY_STORAGE = {"CLOUDINARY_URL": os.environ.get("CLOUDINARY_URL")}
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
