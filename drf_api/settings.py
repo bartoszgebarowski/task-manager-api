76919a78f395f633004a79455ce628f5f8ac8be6
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-02-16 19:26:03 +0100

    remove cloudinary and add static files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -33,9 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "cloudinary_storage",
     "django.contrib.staticfiles",
-    "cloudinary",
     "rest_framework",
     "rest_framework_simplejwt",
     "profiles",
@@ -113,7 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
